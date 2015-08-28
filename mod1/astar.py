4da702e859799b2cf8f21f1a6f64a850e90e9c24
mrton <ton.mangmee@gmail.com>
2015-08-28 18:00:42 +0200

    fixed the print with reversed and added some template code

--- mod1/astar.py
+++ mod1/astar.py
@@ -2,11 +2,30 @@ import sys
 import re
 inFile = sys.argv[1]
 
+
+class Node(object):
+    def __init__(self, state, g_cost, h_cost, status, parent, kids):
+        self.state = state
+        self.g_cost = g_cost
+        self.h_cost = h_cost
+        self.f_cost = self.g_cost + self.h_cost
+        self.status = status
+        self.parent = parent
+        self.kids = kids
+
+
+class A_star_search(object):
+    def __init__(self):
+        self.openlist = []
+        self.closedlist = []
+
+
+
 class Map:
     def __init__(self, width, height, start, goal, walls):
-        self.grid = [['  ' for i in range(width)] for i in range(height)]
-        self.grid[start[1]][start[0]] = 'S '
-        self.grid[goal[1]][goal[0]] = 'G '
+        self.grid = [['.' for i in range(width)] for i in range(height)]
+        self.grid[start[1]][start[0]] = 'S'
+        self.grid[goal[1]][goal[0]] = 'G'
 
         for i in walls:
             basex = i[0]
@@ -16,10 +35,10 @@ class Map:
             wallheight = i[3]
             for x in range(wallwidth):
                 for y in range(wallheight):
-                    self.grid[basey+y][basex+x] = '##'
+                    self.grid[basey+y][basex+x] = '#'
 
     def printMap(self):
-        for i in self.grid:
+        for i in reversed(self.grid):
             print '|',
             for y in i:
                 print y,
