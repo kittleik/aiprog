7045d86bc7e935b87b2232613a4c54284db29ca7
mrton <ton.mangmee@gmail.com>
2015-09-24 14:20:21 +0200

    fiksa domfilter

--- mod2/astar-gac.py
+++ mod2/astar-gac.py
@@ -30,7 +30,13 @@ class GAC:
 		constraint = self.graph.constraints[c]
 		var_names = constraint[0]
 		func = constraint[1]
-		return self.reduced(x, var_names, func)
+		result = self.reduced(x, var_names, func)
+		reduced = False
+		for r in result:
+			if r == False:
+				reduced = True
+				break
+		return (reduced,result)
 
 
 	def reduced(self, x, var_names, func):
@@ -69,12 +75,34 @@ class GAC:
 		while len(todoRevise) > 0:
 			x, c = todoRevise.pop(0)
 			result = self.revise(x, c)
-			todoDelete[x] = result
+			reduced = result[0]
+			removes = result[1]
+			domain = self.graph.domains[x]
+			if reduced:
+				newDomain = self.getNewDomainValues(domain,removes)
+				self.graph.domains[x] = newDomain
+
+		print self.graph.domains
+
+
+
+		#print todoDelete
+		'''
 		res = {}
 		for key in todoDelete:
 			res[key] = deleteMerge(todoDelete[key],self.graph.domains[key])
 		print todoDelete
+		print "----"
 		print res
+		'''
+
+	def getNewDomainValues(self,domain,removes):
+		newdom = []
+		for r in range(len(removes)):
+			if removes[r]:
+				d_value = domain[r]
+				newdom.append(d_value)
+		return newdom
 
 	def makefunc(self, var_names, expression, envir=globals()):
 		args = ""
@@ -108,11 +136,17 @@ ixy = instructions[1:nv+1]
 # [index_of_neighbour1, index_of_neighbour2]
 edges = instructions[nv+2:]
 
-domain = [0,2,3]
+domain = [0,1,2,3,4,5]
 g = Graph(ixy,edges,domain)
 
-#g.domains["n18"] = [1]
+g.domains["n18"] = [1]
+g.domains["n12"] = [2]
+g.domains["n11"] = [3]
+g.domains["n10"] = [4]
+g.domains["n15"] = [5]
+g.domains["n13"] = [0]
 gac = GAC(g)
+print gac.graph.neighbors
 
 
 
@@ -127,12 +161,48 @@ print a
 print gac.graph.domains
 '''
 
+'''
+
+delete = {}
+delete["n10"] = [True,True,False]
+
+domains = {}
+domains["n10"] = [1,2,3]
+
+print delete
+print domains
+
+deln10 = delete.get("n10")
+domn10 = domains.get("n10")
+print domn10
+
+newdom = []
+for d in range(len(deln10)):
+	if deln10[d]:
+		d_value = domn10[d]
+		newdom.append(d_value)
+print newdom
+domains["n10"] = newdom
+
+domn10 = domains.get("n10")
+
+print deln10
+print domn10
+
+hei = ([0],[True,True,False])
+
+h = hei[0]
+e = hei[1]
+
+print h
+print e
+hei[1][0] = 0
+hei[0][0] = 1
+
+print hei
 
 
-
-
-
-
+'''
 
 
 
