fd08aed858a56874e21db7d2151f0e2a1d6eebd8
Håvard <sirkittelsen@gmail.com>
2015-10-05 22:06:42 +0200

    hei

--- mod3/gac.py
+++ mod3/gac.py
@@ -11,27 +11,39 @@ class GAC:
     def construct_domains(self):
         domains = {}
         print self.columns[0]
-        #For every Column
+        print self.columns[1]
+            #For every Column
         for i in range(self.columns[0]):
-            domains["c_"+str(i)] = 0
+            print "-----------------"
+            domains["c_"+str(i)] = []
             print "c_"+str(i)
             min_tiles = len(self.columns[1][i])-1
             #getting minimum length of tiles for instructions
-            print self.columns[1][i]
+            index_addition = 0
             for l in self.columns[1][i]:
                 min_tiles += l
-            print min_tiles
-            #For every instruction
-            for z in range(len(self.columns[1][i])):
-                #For every movable spaces in the tiles
-                for y in range(self.columns[0]-min_tiles):
-                    #For every possible d
-                    for x in range(len(self.columns[1][i])):
-                        domains["c_"+str(i)] = int(domains["c_"+str(i)])+1
-                        print (i,z,y,x)
-                        print "column number " + str(i)
-                        print "instruction number " +str(z)
-                        print "open space " + str(y)
-                        print "asd"
+                #For every instruction
+            for x in range(len(self.columns[1][i])):
+                dl = []
+                y = 0
+                for y in range(self.columns[0]-min_tiles+1):
+
+                    dl.append(y+index_addition)
+                    y+=1
+                    #For every valid spot
+                    #for y in range (self.columns[0]-min_tiles+1)
+
+                domains["c_"+str(i)].append(dl)
+                index_addition += self.columns[1][i][x]
+            self.calculate_domain(domains["c_"+str(i)])
 
         return domains
+
+    def calculate_domain(self, dl):
+        res = []
+        for n in range(len(dl)):
+            for i in range(n,len(dl[n])-n):
+                for x in range (len(dl[n])-i):
+                    res.append([])
+                    for y in range()
+                    print (dl[n][i],dl[n][x])
