50374a020943c39809b5487df55bc1c4af67928e
Ton <Ton@cherdsaks-Mac-mini.local>
2015-10-13 22:32:48 +0200

    up and down works

--- mod4/board.py
+++ mod4/board.py
@@ -1,16 +1,26 @@
 from cell import Cell
+import numpy as np
 
-
+points = 0
 
 class Board():
 
     def __init__(self):
-        self.grid = [[0,0,3,0],[1,0,2,0],[1,0,0,0],[1,0,2,0]]
+        self.grid = [[1,0,2,0],[0,0,0,0],[0,0,0,0],[1,0,2,0]]
         self.state = self.generateState(self.grid)
         #self.rows = [[0, 1, 2, 3],[4, 5, 6, 7],[8, 9, 10, 11],[12, 13, 14, 15]]
         #self.columns = [[0, 4, 8, 12],[1, 5, 9, 13],[2, 6, 10, 14],[3, 7, 11, 15]]
 
 
+    def getColumns(self):
+        grid = np.array(list(self.grid))
+        columns = []
+        for i in range(len(grid)):
+            columns.append(grid[:,i])
+        return np.array(columns).tolist()
+
+    def getRows(self):
+        return list(self.grid)
 
 
     def generateState(self,grid):
@@ -20,13 +30,99 @@ class Board():
                 state.append(element)
         return state
 
-    def swipeUp(self):
-        for column in self.columns:
-            for position in column:
-                print "hei"
+    def swipeUp(self, grid):
+        i = 0
+        for j in range(0,4):
+            if grid[i][j] != 0 or grid[i+1][j] != 0 or grid[i+2][j] != 0 or grid[i+3][j] != 0:
+                if grid[i][j] == 0:
+                    while grid[i][j] == 0:
+                        grid[i][j] = grid[i+1][j]
+                        grid[i+1][j] = grid[i+2][j]
+                        grid[i+2][j] = grid[i+3][j]
+                        grid[i+3][j] = 0
 
+                if grid[i+1][j] == 0 and (grid[i+2][j] != 0 or grid[i+3][j] != 0):
+                    while grid[i+1][j] == 0:
+                        grid[i+1][j] = grid[i+2][j]
+                        grid[i+2][j] = grid[i+3][j]
+                        grid[i+3][j] = 0
 
+                if grid[i+2][j] == 0 and grid[i+3][j] != 0:
+                    while grid[i+2] == 0:
+                        grid[i+2][j] = grid[i+3][j]
+                        grid[i+3][j] = 0
+        print grid
+        return grid
 
+    def upAddition(self, grid):
+        i = 0
+        global points
+        for j in range(0,4):
+            if grid[i][j] == grid[i+1][j]:
+                grid[i][j] = grid[i][j] + grid[i+1][j]
+                points += grid[i][j] ** 2
+                grid[i+1][j] = grid[i+2][j]
+                grid[i+2][j] = grid[i+3][j]
+                grid[i+3][j] = 0
+
+            if grid[i+1][j] == grid[i+2][j]:
+                grid[i+1][j] = grid[i+1][j] + grid[i+2][j]
+                points += grid[i+1][j] ** 2
+                grid[i+2][j] = grid[i+3][j]
+                grid[i+3][j] = 0
+
+            if grid[i+2][j] == grid[i+3][j]:
+                grid[i+2][j] = grid[i+2][j] + grid[i+3][j]
+                points += grid[i+1][j] ** 2
+                grid[i+3][j] = 0
+        print grid
+
+    def swipeDown(self, grid):
+        i = 0
+        for j in range(0,4):
+            if grid[i][j] != 0 or grid[i+1][j] != 0 or grid[i+2][j] != 0 or grid[i+3][j] != 0:
+                if grid[i+3][j] == 0:
+                    while grid[i+3][j] == 0:
+                        grid[i+3][j] = grid[i+2][j]
+                        grid[i+2][j] = grid[i+1][j]
+                        grid[i+1][j] = grid[i][j]
+                        grid[i][j] = 0
+
+                if grid[i+2][j] == 0 and (grid[i+1][j] != 0 or grid[i][j] != 0):
+                    while grid[i+2][j] == 0:
+                        grid[i+2][j] = grid[i+1][j]
+                        grid[i+1][j] = grid[i][j]
+                        grid[i][j] = 0
+
+                if grid[i+1][j] == 0 and grid[i][j] != 0:
+                    while grid[i+1] == 0:
+                        grid[i+1][j] = grid[i][j]
+                        grid[i][j] = 0
+        print grid
+        return grid
+
+    def downAddition(self, grid):
+        i = 0
+        global points
+        for j in range(0,4):
+            if grid[i+3][j] == grid[i+2][j]:
+                grid[i+3][j] = grid[i+3][j] + grid[i+2][j]
+                points += grid[i][j] ** 2
+                grid[i+2][j] = grid[i+1][j]
+                grid[i+1][j] = grid[i][j]
+                grid[i][j] = 0
+
+            if grid[i+2][j] == grid[i+1][j]:
+                grid[i+2][j] = grid[i+2][j] + grid[i+1][j]
+                points += grid[i+2][j] ** 2
+                grid[i+1][j] = grid[i][j]
+                grid[i][j] = 0
+
+            if grid[i+1][j] == grid[i][j]:
+                grid[i+1][j] = grid[i+1][j] + grid[i][j]
+                points += grid[i+1][j] ** 2
+                grid[i][j] = 0
+        print grid
 
 '''
     def checkNeighbour(self, direction):
--- mod4/main.py
+++ mod4/main.py
@@ -1,24 +1,24 @@
 from visuals import GameWindow
 from board import Board
+import time
 
 window = GameWindow()
 
 
 b = Board()
 #b.checkNeighbour("up")
-board1 = b.generateState(b.grid)
+#print "columns: " + str(b.getColumns())
+#print "rows: " + str(b.getRows())
+print b.grid
+g1 = b.swipeUp(b.grid)
+b.upAddition(b.grid)
+window.update_view( b.generateState(g1) )
+#g2 = b.swipeDown(b.grid)
+#b.downAddition(b.grid)
+#window.update_view( b.generateState(g2) )
+print g1
+#print g2
 
-
-board = [   # A list of values currently present in the board on the form 2^x.
-            # Eg: the number 4 implies that the graphical board should display,
-            # 2^4 = 16, the digit 16. This board represents the board in the screen dump below.
-            0, 2, 4, 4,
-            0, 2, 1, 3,
-            0, 1, 1, 3,
-            0, 0, 2, 1
-        ]
-
-
-window.update_view( board1 )
+window.update_view( b.generateState(b.grid) )
 
 window.mainloop()
