67805a3cb412cc471421bd5a5338eab7a6d21527
Håvard <sirkittelsen@gmail.com>
2015-11-24 07:45:44 +0100

    last commit

--- mod5/main.py
+++ mod5/main.py
@@ -1,7 +1,14 @@
-from ann import ANN as ann
+import ann
 import mnist_basics as mnist
 
-import theano
-import theano.tensor as T
-import theano.tensor.nnet as nnet
-import numpy as np
+
+EPOCHS_PER_GAME             = 3
+BATCH                       = 100
+NEURONS_IN_HIDDEN_LAYERS    = [784,500,500,10]
+LIST_OF_FUNCTIONS           = ["rectify","rectify","softmax"]
+LEARNING_RATE               = 0.001
+MOMENTUM_RATE               = 0.9
+
+a = ann(neuronsInHiddenLayers=NEURONS_IN_HIDDEN_LAYERS, listOfFunctions=LIST_OF_FUNCTIONS, learningRate=LEARNING_RATE, momentumRate=MOMENTUM_RATE, errorFunc="RMSprop")
+a.run(BATCH,EPOCHS_PER_GAME)
+mnist.minor_demo(a)
